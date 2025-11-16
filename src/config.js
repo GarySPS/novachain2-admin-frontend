f87b56ffb1fab26ce6cb1a231dd6f6939184f43e
GarySPS <layngelngel96@gmail.com>
2025-11-16 10:28:26 +0630

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-export const API_BASE = import.meta.env.VITE_ADMIN_API_BASE || "https://novachain-admin-backend.onrender.com";
-export const MAIN_API_BASE = import.meta.env.VITE_MAIN_API_BASE || "https://novachain-backend.onrender.com";
+export const API_BASE = import.meta.env.VITE_ADMIN_API_BASE || "https://novachain2-admin-backend.onrender.com";
+export const MAIN_API_BASE = import.meta.env.VITE_MAIN_API_BASE || "https://novachain2-backend.onrender.com";
